1c37be85e388ab5f1429356dc51f93adf77f2d51
Mobassera Zaman <mobassera.zaman@mail.mcgill.ca>
2025-02-05 22:35:21 -0500

    fix route

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -3,6 +3,6 @@ const taskRouter = express.Router();
 const { createTask, deleteTask, updateTask, getTasks} = require("../controllers/taskController");
 
 taskRouter.route("/").get(getTasks).post(createTask);
-taskRouter.route(":/id").put(updateTask).delete(deleteTask);
+taskRouter.route("/:id").put(updateTask).delete(deleteTask);
 
 module.exports = taskRouter;
