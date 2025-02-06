acaa22c84bde30c26b541a6721503db14431c17d
Mobassera Zaman <mobassera.zaman@mail.mcgill.ca>
2025-02-06 14:25:48 -0500

    Setup mongodb for tasks

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,28 +1,39 @@
-const todos = [];
-let id = 1;
+const User = require("../models/userModel");
 
-const createTask = (req, res) => {
+const createTask = async (req, res) => {
+    const username = req.username;
+    const user = await User.findOne({username});
     const { title, completed = false } = req.body;
-    const newTodo = { id: id++, title, completed};
-    todos.push(newTodo);
-    res.status(201).json(newTodo);
-    console.log(todos);
+    const newTodo = { title, completed};
+    user.tasks.push(newTodo);
+    await user.save();
+    res.status(201).json(user);
 }
 
-const updateTask = (req, res) => {
-    const todo = todos.find(todo => todo.id === Number(req.params.id));
-    const {title, completed} = req.body;
+const updateTask = async (req, res) => {
+    const username = req.username;
+    const user = await User.findOne({username});
+    const todo = user.tasks.find(task => task._id == req.params.id);
+    const { title, completed } = req.body;
     todo.title = title;
     todo.completed = completed;
-    res.json(todo);
+    await user.save();
+    res.status(201).json(todo);
 }
 
-const deleteTask = (req, res) => {
-    const todo = todos.findIndex(todo => todo.id === Number(req.params.id));
-    const deletedTodo = todos.splice(todo, 1);
-    res.json(deletedTodo);
+const deleteTask = async (req, res) => {
+    const username = req.username;
+    const user = await User.findOne({username});
+    const index = user.tasks.findIndex(task => task._id == req.params.id);
+    user.tasks.splice(index, 1);
+    await user.save();
+    res.json(user);
 }
 
-const getTasks = (req, res) => res.json(todos);
+const getTasks = async (req, res) => {
+    const username = req.username;
+    const user = await User.findOne({username});
+    res.json(user.tasks)
+}
 
 module.exports = { createTask, updateTask, deleteTask, getTasks};
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const protect = (req, res, next) => {
     const token = req.headers.authorization;
     const decoded = jwt.verify(token, 'secret');
+    req.username = decoded.username;
     next();
 }
 
